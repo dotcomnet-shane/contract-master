84bd1e6caa9b7fca1ae0d4642907a67519efd1ea
struhrmund <shane@dotcomnet.co.za>
2023-08-28 15:20:57 +0200

    This is a test commit 5

--- test6.php
+++ test6.php
@@ -1,3 +1,3 @@
 <?php
 
-echo "Wow I cant believe we have gotten this far! Look how far we've come we've made it!";
+echo "Wow I cant believe we have gotten this far! Look how far we've come we've made it! See how far we've come";
